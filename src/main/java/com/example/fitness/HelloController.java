f1f4b1d102146653c6fe2a5c2199f3a48643a0d2
gabe-tommi <gabetgal2112@gmail.com>
2025-10-14 13:08:03 -0700

    routes now return. packages are correctly named. added controller and test files.

--- src/main/java/com/example/fitness/HelloController.java
+++ src/main/java/com/example/fitness/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.example.fitness;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return ("");
+		return "Hello";
 	}
 }
